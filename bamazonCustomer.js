730dfd915ecdee3140cb421a4348945400b1c0d6
Jihanna02 <jihanna02@gmail.com>
2018-01-11 20:20:45 -0500

    added Inquirer function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,7 +11,64 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
-//functions that do stuff
+//functions that do stuff!
+//function to display inventory DB and prompt
+function startBamazon() {
+  var inventory = [];
+
+  connection.query("SELECT * FROM products", function (err, result, fields) {
+    if (err) throw err;
+
+    inventory = result;
+
+    for(var i = 0; i < inventory.length; i++){
+      console.log("Product ID: " + result[i].item_id);
+      console.log("Product Name: " + result[i].product_name);
+      console.log("Price: " + result[i].price);
+      console.log("-------------");
+    }
+    runInquirer();
+  });
+
+}
+
+//function to run Inquirer
+function runInquirer() {
+
+
+  inquirer.prompt([
+      {
+      name: "productID",
+      type: "input",
+      message: "What is the ID of the product you'd like to buy?"}, 
+        
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many?"
+      }
+    ]).then(function(answer){
+
+      var productID = answer.productID;
+      var quantity = answer.quantity;
+
+      checkInventory( productID, quantity);
+    });
+}
+
+//function to check inventory, p = product ID, q = quantity 
+function checkInventory(p,q) {
+  //if there's no inventory print message and prevent order
+  //else run function to fill order
+
+  fillOrder(n);
+}
+
+//function to fill order
+function fillOrder(n) {
+ //update the database
+ //show customer the total cost
+}
 
 //Connect to Database
 // connect to the mysql server and sql database
@@ -20,5 +77,5 @@ connection.connect(function(err) {
     console.log(err);
   }
   // run the start function after the connection is made to prompt the user
-  console.log("connected");
+  startBamazon();
 });
